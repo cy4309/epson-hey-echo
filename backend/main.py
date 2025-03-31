401e23084343b241169dc5b6890e002ded5d08e5
Chun Lin Pan <wdbps93206@gmail.com>
2025-03-31 16:34:45 +0800

    fix: main.py
    fix genai

--- backend/main.py
+++ backend/main.py
@@ -8,15 +8,17 @@ from reportlab.pdfgen import canvas
 from reportlab.lib.utils import ImageReader
 from openai import OpenAI
 from backend.s3_uploader import upload_to_epsondest
+from google import genai
 
 import uuid
 import os
-import google.generativeai as genai
+# import google.generativeai as genai
 from PIL import Image
 
 # 測試chatbot
 client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
-genai.configure(api_key=os.getenv("GEMINI_API_KEY"))
+# genai.configure(api_key=os.getenv("GEMINI_API_KEY"))
+client = genai.Client(api_key='GEMINI_API_KEY')
 
 app = FastAPI()
 UPLOAD_DIR = "uploads"
@@ -52,7 +54,11 @@ async def generate_prompt(req: Request):
 
         # gemini_response = gemini_model.generate_content(combined_text)
         model = genai.GenerativeModel("gemini-pro")
-        gemini_response = model.generate_content(combined_text)
+        # gemini_response = model.generate_content(combined_text)
+        response = client.models.generate_content(
+        model='gemini-2.0-flash', contents='How does RLHF work?'
+        )
+        print(response.text)
         idea_description = gemini_response.text.strip()
 
         # Step 2: 使用 GPT 生成 prompt
