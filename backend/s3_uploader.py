20397e0e7a82d83cf947e1f79800c5d5ae524437
Chun Lin Pan <wdbps93206@gmail.com>
2025-04-07 16:36:44 +0800

    fix: upload.py

--- backend/s3_uploader.py
+++ backend/s3_uploader.py
@@ -5,21 +5,23 @@ def upload_to_epsondest(filepath: str, filename: str):
     headers = {
         "Authorization": "b1f7690c-ad05-4416-8c42-72df5c38fae2"
     }
+    with open(filepath, "rb") as f:
+        files = {
+            # "file": (filename, open(filepath, "rb")),
+            "file": (filename, f),
+            "fileName": (None, filename),
+            "suffix": (None, f".{filename.split('.')[-1]}")
+        }
 
-    files = {
-        "file": (filename, open(filepath, "rb")),
-        "fileName": (None, filename),
-        "suffix": (None, f".{filename.split('.')[-1]}")
-    }
+        response = requests.post(url, headers=headers, files=files)
+        try:
+            result = response.json()
+        except Exception as e:
+                print("[ERROR] Epson 上傳過程錯誤:", e)
+                return 500, {"error": str(e)}
+        
+        if result.get("Code") != 10000 or not result.get("Data") or result.get("Data") == "null":
+                print("[ERROR] Epson API 回傳錯誤或空資料:", result)
+                return 400, result
 
-    response = requests.post(url, headers=headers, files=files)
-    try:
-        result = response.json()
-    except Exception as e:
-        print("[ERROR] Epson 回傳非 JSON:", response.text)
-        return None
-    if result.get("Code") != 10000 or not result.get("Data") or result.get("Data") == "null":
-        print("[ERROR] Epson API 回傳錯誤或空資料:", result)
-        return None
-
-    return result["Data"]  
+        return 200, result["Data"]  
