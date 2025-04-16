2b13b1726ec6528efed74e3649997ddb76958b3f
Chun Lin Pan <wdbps93206@gmail.com>
2025-04-16 13:18:59 +0800

    fix:fonsize

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -43,7 +43,7 @@ const Home = () => {
   const [isOpenForm, setIsOpenForm] = useState(false);
   const [fileName, setFileName] = useState("");
   const [textContent, setTextContent] = useState("abc");
-  const [fontSize, setFontSize] = useState(30);
+  const [fontSize, setFontSize] = useState(60);
   // const [imgUrls, setImgUrls] = useState([]);
 
   const containerRef = useRef(null);
